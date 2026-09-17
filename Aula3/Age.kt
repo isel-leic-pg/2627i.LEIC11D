b1ceeb2cd38002e95f207a82452528112e91af4b
Pedro Pereira <593033+palex65@users.noreply.github.com>
2026-09-17 14:38:16 +0100

    Aula 17Set - Tipos: Reais & Texto

---
+++ Aula3/Age.kt
@@ -0,0 +1,6 @@
+fun main() {
+  print("Age? ")
+  val line = readln()
+  val age = line.toInt()
+  println("age+1 = ${age+1}")
+}
---
+++ Aula3/Real.kt
@@ -0,0 +1,7 @@
+fun main() {
+	println("Valores reais")
+	val price: Double = 5.3
+	println("Preço = $price")
+	val price2 = price - 0.1
+	println("Preço final = $price2")
+}
